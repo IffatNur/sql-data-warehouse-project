515b19ed1b11f577ae35250303f2aa11b1066d04
Iffat Nur <108322023+IffatNur@users.noreply.github.com>
2025-09-25 12:08:17 +0600

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,25 @@
+use master;
+go
+
+-- drop and recreate DataWarehouse
+if exists(select 1 from sys.databases where name = 'DataWarehouse')
+begin 
+	alter database DataWarehouse set single_user with rollback immediate;
+	drop database DataWarehouse;
+end;
+go
+
+-- create database DataWarehouse
+create database DataWarehouse;
+go
+
+use DataWarehouse;
+go
+
+-- create schema bronze, silver and gold
+create schema bronze;
+go 
+create schema silver;
+go 
+create schema gold;
+go
